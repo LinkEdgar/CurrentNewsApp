3e2912c175c4d45e928a5df9c437def71d61a0cf
Linkedgar <Zenith@Ee0530>
2017-08-24 18:10:59 -0400

    added TODOs and added the the URL where I will get my news from

--- app/src/main/java/com/example/enduser/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/enduser/newsapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.enduser.newsapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -8,6 +9,8 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    //Gets the latest news from the verge
+    private String API_WEB_ADDRESS = " https://newsapi.org/v1/articles?source=the-verge&sortBy=top&apiKey=2ba3568a935d47f4971bfa683e064bdb";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
         listView.setAdapter(adapter);
 
+
+    }
+    public class NetworkAsyncTask extends AsyncTask<String, Void, Void>{
+
+        @Override
+        protected Void doInBackground(String... params) {
+            //TODO
+            //if the string is valid we will run the network utils on it
+            return null;
+        }
+        //TODO update our UI method
 
     }
 }
--- app/src/main/java/com/example/enduser/newsapplication/NetworkUtils.java
+++ app/src/main/java/com/example/enduser/newsapplication/NetworkUtils.java
@@ -11,19 +11,25 @@ public class NetworkUtils {
     //takes a url and connects it, if the connection is successful it returns a string version of the  JSON object
 
     public String makeHttpConnection(URL url){
-
+        //TODO complete method
         return  null;
     }
     //Reads the input stream and returns a string of the input stream
     public String readInputStream(){
+        //TODO complete method
+
         return null;
     }
     // converts a string into a url
     public URL stringToUrl(String stringUrl){
+        //TODO complete method
+
         return null;
     }
     //extracts all of the info we need from the JSON string and returns an ArrayList of a NewsObject type
     public ArrayList<NewsObject> extractJsonInformation(){
+        //TODO complete method
+
         return null;
     }
     //TODO make a method that brings all of the newtwork utils together
