aea500886a1aa249494239eabad99a3e5fb9abc0
Linkedgar <Zenith@Ee0530>
2017-08-24 17:47:14 -0400

    Created a network utils class to handle all of the network code

---
+++ app/src/main/java/com/example/enduser/newsapplication/NetworkUtils.java
@@ -0,0 +1,32 @@
+package com.example.enduser.newsapplication;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by EndUser on 8/24/2017.
+ */
+
+public class NetworkUtils {
+    //takes a url and connects it, if the connection is successful it returns a string version of the  JSON object
+
+    public String makeHttpConnection(URL url){
+
+        return  null;
+    }
+    //Reads the input stream and returns a string of the input stream
+    public String readInputStream(){
+        return null;
+    }
+    // converts a string into a url
+    public URL stringToUrl(String stringUrl){
+        return null;
+    }
+    //extracts all of the info we need from the JSON string and returns an ArrayList of a NewsObject type
+    public ArrayList<NewsObject> extractJsonInformation(){
+        return null;
+    }
+    //TODO make a method that brings all of the newtwork utils together
+    //this method brings it all together for one call
+
+}
